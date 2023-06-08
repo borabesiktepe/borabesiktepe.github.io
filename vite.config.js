a876fd741a20dd726ca03230cba565cae48cbe88
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-06-09 01:10:09 +0300

    yarrak

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/borabesiktepe.github.io/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
         mimeTypes: {
           'text/x-typescript': ['ts', 'tsx'],
           'text/javascript': ['js', 'jsx'],
+          'text/jsx': ['js', 'jsx'],
         }
       }
     }
