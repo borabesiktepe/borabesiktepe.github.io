5c132142a74648ace4f9d2a569d3413ac40d43df
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-06-09 01:04:31 +0300

    dklsgm

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/borabesiktepe.github.io/',
+  build: {
+    rollupOptions: {
+      output: {
+        mimeTypes: {
+          'text/x-typescript': ['ts', 'tsx'],
+          'text/javascript': ['js', 'jsx'],
+        }
+      }
+    }
+  }
 })
