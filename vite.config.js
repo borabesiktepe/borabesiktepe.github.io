b54171820125244db49000394fbd48ca7df06585
Fezalion <kerimgileren@gmail.com>
2023-06-09 02:24:38 +0300

    testing stuff

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      output: {
-        mimeTypes: {
-          'text/x-typescript': ['ts', 'tsx'],
-          'text/javascript': ['js', 'jsx'],
-          'text/jsx': ['js', 'jsx'],
-        }
-      }
-    }
-  }
+  plugins: [react()],  
 })
