fbcaf3c0ad92f41a8a75c30258ff56840f8fa46c
Fezalion <Fezalion@users.noreply.github.com>
2023-06-08 23:29:02 +0000

    deploy: 24dc4a30729eae756bf54e99d328479466dc7c3e

---
+++ assets/index-3658026a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-b3c06aa4.js";function n(){return e.jsx(e.Fragment,{children:e.jsxs("footer",{children:[e.jsx("span",{children:"#1A1A1A // #E7E7E7"}),e.jsx("span",{children:"© 2023 Bora Beşiktepe"})]})})}export{n as default};
