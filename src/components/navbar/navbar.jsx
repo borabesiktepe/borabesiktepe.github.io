4f42caaff6596e875a296b8c44e4963e45beb1fb
Fezalion <kerimgileren@gmail.com>
2023-06-09 16:54:24 +0300

    framer-ish

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion , domAnimation } from "framer-motion"
+import { motion , domAnimation, LazyMotion, m } from "framer-motion"
 
 import { NavLink } from "react-router-dom";
 
@@ -13,8 +13,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
-    setIsOpen(!isOpen);    
-    console.log('clicked:' + isOpen);
+    setIsOpen(!isOpen);
   };
 
   const [isHovered, setIsHovered] = useState(false);
@@ -41,23 +40,38 @@ function Navbar() {
     setIsHovered3(false);
   };
 
+  const show = {
+    opacity: 1,
+    transition: {
+      duration: 0,
+    }
+  };
+  
+  const hide = {
+    opacity: 0,
+    transition: {
+      duration: 0,
+    }
+    
+  };
+
   return (
-    <>
+    <LazyMotion features={domAnimation}>
       <header>
-          <motion.p>Bora Beşiktepe</motion.p>
-          <motion.div className="menu-button" onClick={handleToggle}>
+          <m.p>Bora Beşiktepe</m.p>
+          <m.div className="menu-button" onClick={handleToggle}>
               <svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 0H18V2H0V0ZM0 7H12V9H0V7ZM0 14H18V16H0V14Z" fill="#E7E7E7"/>
               </svg>
             <span>Menu</span>
-          </motion.div>          
+          </m.div>          
       </header>
-      <motion.div className="background"
+      <m.div className="background"
           initial={{ x: "-100%" }}
           animate={{ x: isOpen ? "0%" : "-100%" }}
           transition={{ duration: 0 }}
-          onClick={handleToggle}></motion.div>
-      <motion.div
+          onClick={handleToggle}></m.div>
+      <m.div
         className="menu"
         initial={{ x: "-100%" }}
         animate={{ x: isOpen ? "0%" : "-100%" }}
@@ -66,14 +80,23 @@ function Navbar() {
         <ul>
           <p>Bora Beşiktepe</p>
           <div className="menu-items">
-            <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{isHovered && <img src={Hover1} className="hov hover1" />} <NavLink to="/">ARTWORKS</NavLink></li>
-            <li onMouseEnter={handleMouseEnter2} onMouseLeave={handleMouseLeave2}>{isHovered2 && <img src={Hover2} className="hov hover2" />} <NavLink to="/about">ABOUT</NavLink></li>
-            <li onMouseEnter={handleMouseEnter3} onMouseLeave={handleMouseLeave3}>{isHovered3 && <img src={Hover3} className="hov hover3" />}<NavLink to="/">CONTACT</NavLink></li>
+            <m.li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+              <m.img src={Hover1} animate={isHovered ? show : hide} className="hover1 hov" />
+                <NavLink to="/">ARTWORKS</NavLink>
+            </m.li>
+            <m.li onMouseEnter={handleMouseEnter2} onMouseLeave={handleMouseLeave2}>
+              <m.img src={Hover2} animate={isHovered2 ? show : hide} className="hover2 hov" />
+              <NavLink to="/about">ABOUT</NavLink>
+            </m.li>
+            <m.li onMouseEnter={handleMouseEnter3} onMouseLeave={handleMouseLeave3}>
+              <m.img src={Hover3} animate={isHovered3 ? show : hide} className="hover3 hov" />
+              <NavLink to="/">CONTACT</NavLink>
+            </m.li>
           </div>
         </ul>
         <img src={menuImage}></img>
-      </motion.div>
-    </>
+      </m.div>
+    </LazyMotion>
   );
 }
 
