b249366803634119f22045614bb1925c06979ead
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-06-21 19:06:47 +0300

    Updated artwork images

--- src/components/artworks/artworks.jsx
+++ src/components/artworks/artworks.jsx
@@ -4,19 +4,19 @@ function Artworks () {
     return (
         <div className="artworks">
             <div className="artwork-item">
-                <img src="https://cdna.artstation.com/p/assets/images/images/059/700/870/large/bora-besiktepe-309920803-1242645133253186-7848651775024215199-n.jpg?1676969907" alt="Artwork1"></img>
+                <img src="https://i.imgur.com/NOfW88G.png" alt="Artwork1"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://cdnb.artstation.com/p/assets/images/images/059/700/841/large/bora-besiktepe-243716927-591849758833509-4850822869558982970-n.jpg?1676969836" alt="Artwork2"></img>
+                <img src="https://i.imgur.com/wxcZzVd.png" alt="Artwork2"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://cdnb.artstation.com/p/assets/images/images/059/700/809/large/bora-besiktepe-121255888-1018205745270759-8621102770366216297-n.jpg?1676969786" alt="Artwork3"></img>
+                <img src="https://i.imgur.com/EV7wJGa.png" alt="Artwork3"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://cdna.artstation.com/p/assets/images/images/059/700/530/large/bora-besiktepe-185255469-518941912464951-334296113164757913-n.jpg?1676969406" alt="Artwork4"></img>
+                <img src="https://i.imgur.com/mTkh2tz.png" alt="Artwork4"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://cdna.artstation.com/p/assets/images/images/059/700/764/large/bora-besiktepe-137269091-713051319415574-2780865654196107180-n.jpg?1676969732" alt="Artwork5"></img>
+                <img src="https://i.imgur.com/S6Gf6so.png" alt="Artwork5"></img>
             </div>
         </div>
     )
