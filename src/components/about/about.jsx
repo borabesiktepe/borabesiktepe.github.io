39d616c68b2ad59d69bbb7ec939cf3474e33d4fb
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-08-12 04:35:55 +0300

    Updated about page layout

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Menu from '../menu/menu.jsx';
 
 const About = () => {
@@ -11,15 +12,15 @@ const About = () => {
       </nav>
 
       <div className="about-section">
-        <h1>ABOUT</h1>
-
         <div className="about-content">
           <p>Self-taught illustrator and developer from Istanbul, Türkiye.</p>
+
+          <h1>ABOUT</h1>
 
           <div className="about-contacts">
             <p>Want to contact me?</p>
             <ul>
-              <li>borabesiktepe@gmail.com</li>
+              <li><a href="mailto:borabesiktepe@gmail.com">borabesiktepe@gmail.com</a></li>
               <li><a href="https://www.instagram.com/borabesiktepe/" target='_blank'>Instagram</a></li>
               <li><a href="https://github.com/borabesiktepe" target='_blank'>GitHub</a></li>
               <li><a href="https://www.artstation.com/borabesiktepe" target='_blank'>ArtStation</a></li>
@@ -28,6 +29,10 @@ const About = () => {
 
           <p className='about-siteinfo'>This website was developed with the help of React.</p>
         </div>
+      </div>
+
+      <div className="about-background">
+        <img src="https://media.tenor.com/el39oVPZlCIAAAAd/static-noise.gif"/>
       </div>
     </>
   );
