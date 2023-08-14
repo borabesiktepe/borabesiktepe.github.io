d990ac29c153de5c2fedaa43a285d2fbbdfd00bb
Fezalion <kerimgileren@gmail.com>
2023-08-14 20:25:23 +0300

    Add page transitions, remove static gif background.

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,17 @@
+import { motion } from "framer-motion";
+
 import Artworks from '../artworks';
 import Menu from '../menu';
 import {DarkModeToggle} from '../darkmode';
 
 function home() {
-    return (
-        <>
-            <main>
+    return (        
+            <motion.main
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+            >
                 <div className="title">
                     <h1>BORA BEŞİKTEPE</h1>
                     <Menu />
@@ -15,8 +21,7 @@ function home() {
                     <Artworks></Artworks>
                     <span class="desc">Based in İstanbul.</span>
                 </div>
-            </main>
-        </>
+            </motion.main>        
     )
 }
 
