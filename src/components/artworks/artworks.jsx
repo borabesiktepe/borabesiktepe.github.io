8df63568276355129fd2d762d4d3084b5e263779
Fezalion <kerimgileren@gmail.com>
2024-06-03 17:28:25 +0300

    local images part 2

--- src/components/artworks/artworks.jsx
+++ src/components/artworks/artworks.jsx
@@ -4,19 +4,19 @@ function Artworks () {
     return (
         <div className="artworks">
             <div className="artwork-item">
-                <img src="https://i.imgur.com/NOfW88G.png" alt="Artwork1"></img>
+                <img src="/art/NOfW88G.png" alt="Artwork1"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://i.imgur.com/wxcZzVd.png" alt="Artwork2"></img>
+                <img src="/art/wxcZzVd.png" alt="Artwork2"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://i.imgur.com/EV7wJGa.png" alt="Artwork3"></img>
+                <img src="/art/EV7wJGa.png" alt="Artwork3"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://i.imgur.com/mTkh2tz.png" alt="Artwork4"></img>
+                <img src="/art/mTkh2tz.png" alt="Artwork4"></img>
             </div>
             <div className="artwork-item">
-                <img src="https://i.imgur.com/S6Gf6so.png" alt="Artwork5"></img>
+                <img src="/art/S6Gf6so.png" alt="Artwork5"></img>
             </div>
         </div>
     )
